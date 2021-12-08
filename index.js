7bc27b4fed6c175f0eaffb0feacd795f0771e412
TBlackford <TBlackford@users.noreply.github.com>
2021-12-08 22:47:04 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//module.exports = require('./lib/index')
+
+//const trans = require('./lib/index');
+import trans from './lib/index.js';
+
+// Do IPA to Fenwick first.
+console.log(trans({}, 'waantʷan'))
